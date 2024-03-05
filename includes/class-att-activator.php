838dc3bb48176308e9dce0bc0c4fc26052b9ccd0
Esubalew <esubalew.a2009@gmail.com>
2024-03-05 10:01:47 +0300

    plugin basics

---
+++ includes/class-att-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https:///github.com/EsubalewAmenu
+ * @since      1.0.0
+ *
+ * @package    Att
+ * @subpackage Att/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Att
+ * @subpackage Att/includes
+ * @author     Esubalew A <esubalew.a2009@gmail.com>
+ */
+class Att_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-att-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https:///github.com/EsubalewAmenu
+ * @since      1.0.0
+ *
+ * @package    Att
+ * @subpackage Att/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Att
+ * @subpackage Att/includes
+ * @author     Esubalew A <esubalew.a2009@gmail.com>
+ */
+class Att_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
