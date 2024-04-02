9c47c407406895a14280692e58187de11ff699d8
Esubalew <esubalew.a2009@gmail.com>
2024-04-02 11:48:16 +0300

    display array data as table model

--- admin/partials/account/transactions.php
+++ admin/partials/account/transactions.php
@@ -1,1 +1,10 @@
-<h1>Transactions</h1>
+<div class="wrap">
+			<h1>My Transactions </h1>
+			<div id="col-container">
+				<div >
+					<div class="col-wrap">
+						<?php $receiving_addresses_table->display(); ?>
+					</div>
+				</div>
+			</div>
+		</div>
