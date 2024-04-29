9505ccfa11dd0a22fd5410424d8c997eabaa6327
Esubalew <esubalew.a2009@gmail.com>
2024-04-29 13:03:18 +0300

    show message after saved

--- admin/partials/settings/index.php
+++ admin/partials/settings/index.php
@@ -1,8 +1,10 @@
 <div class="wrap">
-        <h2>ADA Tracking Settings</h2>
-        <form method="post" action="options.php">
-            <?php settings_fields('ada_tracking_settings_group'); ?>
-            <?php do_settings_sections('ada-tracking-plugin'); ?>
-            <?php submit_button('Save Settings'); ?>
-        </form>
-    </div>
+    <h2>ADA Tracking Settings</h2>
+
+    <?php settings_errors(); ?>
+    <form method="post" action="options.php">
+        <?php settings_fields('ada_tracking_settings_group'); ?>
+        <?php do_settings_sections('ada-tracking-plugin'); ?>
+        <?php submit_button('Save Settings'); ?>
+    </form>
+</div>
